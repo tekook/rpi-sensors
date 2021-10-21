698f082b7087859ab6f865a1fdf95cdaf384e7d7
jute <julian@tekook.de>
2021-10-21 21:55:10 +0200

    update

--- sensors.py
+++ sensors.py
@@ -1,22 +1,24 @@
 #! /usr/bin/env python3
 import json
 import sys
-import board
+
 
 settings_file = open('settings.json')
 settings = json.load(settings_file)
 settings_file.close()
+
 if settings['sensors']['ltr390']['enable'] or settings['sensors']['mcp9808']['enable'] or settings['sensors']['sht4x']['enable'] or settings['sensors']['dps310']['enable'] :
-    i2c = board.IC2()
+    import board
+    i2c = board.I2C()
 
 results = []
 warnings = []
 
-if settings.sensors.dht22.enable :
+if settings['sensors']['dht22']['enable'] :
     import Adafruit_DHT
     sensor = Adafruit_DHT.DHT22
     humidity, temperature = Adafruit_DHT.read_retry(sensor,
-    settings.sensors.dht22.pin)
+    settings['sensors']['dht22']['pin'])
     if humidity is not None and temperature is not None:
         results.append( {
             "channel": "Temperature (DHT22)",
@@ -35,20 +37,21 @@ if settings.sensors.dht22.enable :
 
 
 
-if settings.sensors.ds18b20.enable :
-    from adafruit_onewire.bus import OneWireBus
-    from adafruit_ds18x20 import DS18X20
-
-    ow_bus = OneWireBus(board.D2)
-    ds18 = DS18X20(ow_bus, ow_bus.scan()[0])
-    results.append( {
+if settings['sensors']['ds18b20']['enable'] :
+    from w1thermsensor import W1ThermSensor
+    try :
+        ds18b20 = W1ThermSensor()
+        temperature_in_celsius = ds18b20.get_temperature()
+        results.append( {
             "channel": "Temperature (DS18B20)",
             "customunit": "°C",
             "float": 1,
-            "value": float(format(ds18.temperature, "0.4f"))
+            "value": float(format(temperature_in_celsius, "0.4f"))
         })
+    except :
+        warnings.append(str(sys.exc_info()[0]))
 
-if settings.sensors.ltr390.enable :
+if settings['sensors']['ltr390']['enable'] :
     import adafruit_ltr390
     try :
         i2c = board.I2C()
@@ -68,7 +71,7 @@ if settings.sensors.ltr390.enable :
     except :
         warnings.append(str(sys.exc_info()[0]))
 
-if settings.sensors.mcp9808.enable :
+if settings['sensors']['mcp9808']['enable'] :
     import adafruit_mcp9808
     mcp = adafruit_mcp9808.MCP9808(i2c)
     results.append( {
@@ -78,24 +81,25 @@ if settings.sensors.mcp9808.enable :
             "value": float(format(mcp.temperature, "0.4f"))
         })
 
-if settings.sensors.sht4x.enable :
+if settings['sensors']['sht4x']['enable'] :
     import adafruit_sht4x
     sht = adafruit_sht4x.SHT4x(i2c)
     sht.mode = adafruit_sht4x.Mode.NOHEAT_HIGHPRECISION
+    temperature, relative_humidity = sht.measurements
     results.append( {
         "channel": "Temperature (SHT4x)",
         "customunit": "°C",
         "float": 1,
-        "value": float(format(sht.measurements.temperature, "0.4f"))
+        "value": float(format(temperature, "0.4f"))
     })
     results.append({
         "channel": "Humidity (SHT4x)",
         "customunit": "%",
         "float": 1,
-        "value": float(format(sht.measurements.relative_humidity, "0.4f"))
+        "value": float(format(relative_humidity, "0.4f"))
     })
 
-if settings.sensors.dps310.enable :
+if settings['sensors']['dps310']['enable'] :
     import adafruit_dps310
     dps310 = adafruit_dps310.DPS310(i2c)
     results.append( {
@@ -110,6 +114,12 @@ if settings.sensors.dps310.enable :
         "float": 1,
         "value": float(format(dps310.pressure, "0.4f"))
     })
+
+
+
+
+
+
 
 results.append({
     "channel": "Warnings",
@@ -126,4 +136,4 @@ print(json.dumps({
         "result": results,
         "text": text
     }
-}), end='')
+}, sort_keys=True, indent=4), end='')
