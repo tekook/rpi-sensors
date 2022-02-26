55fea3d7a58004cc89db2f67c196b48c320af128
jute <julian@tekook.de>
2022-02-26 17:34:37 +0100

    fix again

--- setup/uninstall.sh
+++ setup/uninstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 systemctl stop rpi-sensors.service ; \
 systemctl disable rpi-sensors.service ; \
-rm /etc/systemd/system/rpi-sensors.service && \
+rm /etc/systemd/system/rpi-sensors.service ; \
 systemctl daemon-reload && \
-rm /etc/nginx/sites-enabled/rpi-sensors.conf && \
+rm /etc/nginx/sites-enabled/rpi-sensors.conf ; \
 rm -R ./venv
