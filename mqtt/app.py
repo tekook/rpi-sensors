227fbb835803d57c5f4d3faa6e0db7b0483d654a
Julian Tekook <julian@tekook.de>
2023-01-26 14:16:53 +0100

    fix formating

--- mqtt/app.py
+++ mqtt/app.py
@@ -1,24 +1,23 @@
 from threading import Event
 import libbase as lib, libsensors as sensors
 
-calls = {
-    'sht4x': {'func': sensors.getSHT4X, 'arg': False},
-    'ltr390': {'func': sensors.getLTR390, 'arg': False,
-    'ds18b20': {'func': sensors.getDS18B20, 'arg': False},
-    'mcp9808': {'func': sensors.getMCP9808, 'arg': False},
-    'dps310': {'func': sensors.getDPS310, 'arg': False},
-    'hub': {'func': sensors.getHUB, 'arg': False}
-}
-
-client=lib.create_client_from_config()
-config=lib.getConfig()
-
-run = True
 exit = Event()
 def quit(signo, _frame):
     print("Interrupted by %d, shutting down" % signo)
     exit.set()
+
 def main():
+    calls = {
+        'sht4x': {'func': sensors.getSHT4X, 'arg': False},
+        'ltr390': {'func': sensors.getLTR390, 'arg': False},
+        'ds18b20': {'func': sensors.getDS18B20, 'arg': False},
+        'mcp9808': {'func': sensors.getMCP9808, 'arg': False},
+        'dps310': {'func': sensors.getDPS310, 'arg': False},
+        'hub': {'func': sensors.getHUB, 'arg': False},
+    }
+
+    client = lib.create_client_from_config()
+    config = lib.getConfig()      
     client.loop_start()
     while not exit.is_set():
         lib.loop(calls,client)
